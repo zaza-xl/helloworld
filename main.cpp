a759522df18ee08fd8de90db9fd2e0d616a6f4af
zaza-xl <xl_19841205@163.com>
2017-03-06 21:45:45 +0800

    add the print code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    printf("Hello World!\n");
 
     return a.exec();
 }
